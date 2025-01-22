30650ba1f06c27bbc9fed79130bddf7d5e1c6cd3
Wihy <105944317+itsWihy@users.noreply.github.com>
2025-01-22 12:26:55 +0200

    Feat: add more flipping for more rotation.

--- src/main/java/frc/lib/util/flippable/FlippableUtils.java
+++ src/main/java/frc/lib/util/flippable/FlippableUtils.java
@@ -2,6 +2,7 @@ package frc.lib.util.flippable;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 
 import static frc.robot.utilities.FieldConstants.FIELD_LENGTH;
 import static frc.robot.utilities.FieldConstants.FIELD_WIDTH;
@@ -38,32 +39,62 @@ public class FlippableUtils {
     }
 
     /**
-     * Rotate the rotation2d about the Y axis
+     * Rotate the rotation about the Y axis
      *
-     * @param rotation2d The rotation2d to rotate
-     * @return The rotated rotation2d
+     * @param rotation2d The rotation to rotate
+     * @return The rotated rotation
      */
     public static Rotation2d flipAboutYAxis(Rotation2d rotation2d) {
         return Rotation2d.k180deg.minus(rotation2d);
     }
 
     /**
-     * Rotate the pose about the X axis, including rotation.
+     * Rotate the rotation about the X axis
      *
-     * @param rotation The pose to rotate
-     * @return The rotated pose
+     * @param rotation The rotation to rotate
+     * @return The rotated rotation
      */
     public static Rotation2d flipAboutXAxis(Rotation2d rotation) {
         return rotation.unaryMinus();
     }
 
     /**
-     * Rotate the pose about the X and Y axes, including rotation.
+     * Rotate the rotation about the X and Y axes.
      *
-     * @param rotation The pose to rotate
-     * @return The rotated pose
+     * @param rotation The rotation to rotate
+     * @return The rotated rotation
      */
     public static Rotation2d flipAboutBothAxis(Rotation2d rotation) {
         return rotation.minus(Rotation2d.kPi);
+    }
+
+    /**
+     * Rotate the translation about the Y axis
+     *
+     * @param translation The translation to rotate
+     * @return The rotated translation
+     */
+    public static Translation2d flipAboutYAxis(Translation2d translation) {
+        return new Translation2d(FIELD_LENGTH - translation.getX(), translation.getY());
+    }
+
+    /**
+     * Rotate the translation about the X axis
+     *
+     * @param translation The translation to rotate
+     * @return The rotated translation
+     */
+    public static Translation2d flipAboutXAxis(Translation2d translation) {
+        return new Translation2d(translation.getX(), FIELD_WIDTH - translation.getY());
+    }
+
+    /**
+     * Rotate the translation about the X and Y axes.
+     *
+     * @param translation The translation to rotate
+     * @return The rotated translation
+     */
+    public static Translation2d flipAboutBothAxis(Translation2d translation) {
+        return new Translation2d(FIELD_LENGTH - translation.getX(), FIELD_WIDTH - translation.getY());
     }
 }
