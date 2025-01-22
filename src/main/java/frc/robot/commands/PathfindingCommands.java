3d2f6b5464a7ba03202696e592e81ef3923021e9
biqsweet <ranranlevyy@gmail.com>
2025-01-23 01:24:24 +0200

    Feat: added Pathfinding to the closest reef face.

--- src/main/java/frc/robot/commands/PathfindingCommands.java
+++ src/main/java/frc/robot/commands/PathfindingCommands.java
@@ -1,10 +1,14 @@
 package frc.robot.commands;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.lib.util.flippable.Flippable;
+import frc.lib.util.flippable.FlippablePose2d;
 import frc.robot.subsystems.swerve.SwerveCommands;
 
 import java.util.Set;
@@ -13,14 +17,47 @@ import static frc.lib.util.flippable.FlippableUtils.flipAboutXAxis;
 import static frc.lib.util.flippable.FlippableUtils.flipAboutYAxis;
 import static frc.robot.RobotContainer.POSE_ESTIMATOR;
 import static frc.robot.RobotContainer.SWERVE;
-import static frc.robot.utilities.FieldConstants.BLUE_BOTTOM_FEEDER_INTAKE_POSE;
-import static frc.robot.utilities.FieldConstants.FIELD_WIDTH;
+import static frc.robot.utilities.FieldConstants.*;
+import static frc.robot.utilities.FieldConstants.ReefFace.*;
 
 public class PathfindingCommands {
     public static void setupFeederPathfinding(Trigger button) {
         button.whileTrue(
                 Commands.defer(PathfindingCommands::pathfindToFeeder, Set.of(SWERVE))
         );
+    }
+
+    public static void setupReefPathfinding(Trigger button1, Trigger button2) {
+        button1.and(button2.negate()).whileTrue(
+                Commands.defer(() -> pathfindToBranch(true), Set.of(SWERVE))
+        );
+
+        button2.and(button1.negate()).whileTrue(
+                Commands.defer(() -> pathfindToBranch(false), Set.of(SWERVE))
+        );
+    }
+
+    private static Command pathfindToBranch(boolean isLeftBranch) {
+        final Pose2d targetPose = isLeftBranch ? decideReefFace().getLeftBranch() : decideReefFace().getRightBranch();
+
+        return isRobotInProximity(targetPose, 0.8) ?
+                SwerveCommands.goToPosePID(targetPose) :
+                SwerveCommands.goToPoseBezier(targetPose);
+    }
+
+    private static ReefFace decideReefFace() {
+        final FlippablePose2d reef = new FlippablePose2d(new Pose2d(REEF_CENTER, Rotation2d.kZero), true);
+        final Translation2d robotPose = POSE_ESTIMATOR.getCurrentPose().getTranslation();
+        final Translation2d distanceToReef = reef.get().getTranslation().minus(robotPose);
+
+        final double angle = Units.radiansToDegrees(Math.atan2(distanceToReef.getY(), distanceToReef.getX()));
+
+        if (-30 <= angle && angle < 30) return Flippable.isRedAlliance() ? FACE_3 : FACE_0;
+        else if (-90 <= angle && angle < -30) return Flippable.isRedAlliance() ? FACE_4 : FACE_1;
+        else if (-150 <= angle && angle < -90) return Flippable.isRedAlliance() ? FACE_5 : FACE_2;
+        else if (150 <= angle || angle < -150) return Flippable.isRedAlliance() ? FACE_0 : FACE_3;
+        else if (90 <= angle) return Flippable.isRedAlliance() ? FACE_1 : FACE_4;
+        else return Flippable.isRedAlliance() ? FACE_2 : FACE_5;
     }
 
     private static Command pathfindToFeeder() {
--- src/main/java/frc/robot/utilities/FieldConstants.java
+++ src/main/java/frc/robot/utilities/FieldConstants.java
@@ -1,6 +1,9 @@
 package frc.robot.utilities;
 
 import edu.wpi.first.math.geometry.*;
+import frc.lib.util.flippable.Flippable;
+
+import static frc.lib.util.flippable.FlippableUtils.flipAboutBothAxis;
 
 public class FieldConstants {
     private static final Transform2d ROBOT_TRANSFORM = new Transform2d(new Translation2d(0.5, 0), Rotation2d.fromDegrees(180));
@@ -10,4 +13,39 @@ public class FieldConstants {
 
     public static final double FIELD_WIDTH = 8.05;
     public static final double FIELD_LENGTH = 17.55;
+
+    public static final Translation2d REEF_CENTER = new Translation2d(4.490, 4.027);
+
+    public enum ReefFace {
+        FACE_0(3.6587, 4.0269, Rotation2d.k180deg),
+        FACE_1(4.0745, 4.7472, Rotation2d.fromDegrees(120)),
+        FACE_2(4.9052, 4.7472, Rotation2d.fromDegrees(60)),
+        FACE_3(5.3210, 4.0259, Rotation2d.kZero),
+        FACE_4(4.9053, 3.3067, Rotation2d.fromDegrees(-60)),
+        FACE_5(4.0746, 3.3064, Rotation2d.fromDegrees(-120));
+
+        private final Pose2d facePose;
+        private final Pose2d leftBranch;
+        private final Pose2d rightBranch;
+
+        ReefFace(double x, double y, Rotation2d rotation) {
+            this.facePose = new Pose2d(x, y, rotation);
+
+            final Pose2d faceDirection = new Pose2d(REEF_CENTER, facePose.getRotation());
+            this.leftBranch = faceDirection.transformBy(new Transform2d(0.7808, -0.1643, Rotation2d.kZero)).transformBy(ROBOT_TRANSFORM);
+            this.rightBranch = faceDirection.transformBy(new Transform2d(0.7808, 0.1643, Rotation2d.kZero)).transformBy(ROBOT_TRANSFORM);
+        }
+
+        public Pose2d getPose() {
+            return Flippable.isRedAlliance() ? flipAboutBothAxis(facePose) : facePose;
+        }
+
+        public Pose2d getLeftBranch() {
+            return Flippable.isRedAlliance() ? flipAboutBothAxis(leftBranch) : leftBranch;
+        }
+
+        public Pose2d getRightBranch() {
+            return Flippable.isRedAlliance() ? flipAboutBothAxis(rightBranch) : rightBranch;
+        }
+    }
 }
