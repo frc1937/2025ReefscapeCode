61c3efa77140a31267673c261303789dfe6190d5
biqsweet <ranranlevyy@gmail.com>
2025-01-23 09:04:30 +0200

    Fix: fixed flippable shenanigans and moved transforms to be constant.

--- src/main/java/frc/robot/utilities/FieldConstants.java
+++ src/main/java/frc/robot/utilities/FieldConstants.java
@@ -1,12 +1,17 @@
 package frc.robot.utilities;
 
-import edu.wpi.first.math.geometry.*;
-import frc.lib.util.flippable.Flippable;
-
-import static frc.lib.util.flippable.FlippableUtils.flipAboutBothAxis;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import frc.lib.util.flippable.FlippablePose2d;
+import frc.lib.util.flippable.FlippableTranslation2d;
 
 public class FieldConstants {
     private static final Transform2d ROBOT_TRANSFORM = new Transform2d(new Translation2d(0.5, 0), Rotation2d.fromDegrees(180));
+
+    private final static Transform2d LEFT_BRANCH_TRANSFORM = new Transform2d(0.7808, -0.1643, Rotation2d.kZero);
+    private final static Transform2d RIGHT_BRANCH_TRANSFORM = new Transform2d(0.7808, 0.1643, Rotation2d.kZero);
 
     public static final Pose2d BLUE_BOTTOM_FEEDER_INTAKE_POSE = new Pose2d(new Translation2d(0.84319, 0.65078),
                     Rotation2d.fromDegrees(54)).transformBy(ROBOT_TRANSFORM);
@@ -14,7 +19,7 @@ public class FieldConstants {
     public static final double FIELD_WIDTH = 8.05;
     public static final double FIELD_LENGTH = 17.55;
 
-    public static final Translation2d REEF_CENTER = new Translation2d(4.490, 4.027);
+    public static final Translation2d REEF_CENTER = new FlippableTranslation2d(4.490, 4.027, true).get();
 
     public enum ReefFace {
         FACE_0(3.6587, 4.0269, Rotation2d.k180deg),
@@ -29,23 +34,23 @@ public class FieldConstants {
         private final Pose2d rightBranch;
 
         ReefFace(double x, double y, Rotation2d rotation) {
-            this.facePose = new Pose2d(x, y, rotation);
+            this.facePose = new FlippablePose2d(x, y, rotation,true).get();
 
             final Pose2d faceDirection = new Pose2d(REEF_CENTER, facePose.getRotation());
-            this.leftBranch = faceDirection.transformBy(new Transform2d(0.7808, -0.1643, Rotation2d.kZero)).transformBy(ROBOT_TRANSFORM);
-            this.rightBranch = faceDirection.transformBy(new Transform2d(0.7808, 0.1643, Rotation2d.kZero)).transformBy(ROBOT_TRANSFORM);
+            this.leftBranch = new FlippablePose2d(faceDirection.transformBy(LEFT_BRANCH_TRANSFORM).transformBy(ROBOT_TRANSFORM), true).get();
+            this.rightBranch = new FlippablePose2d(faceDirection.transformBy(RIGHT_BRANCH_TRANSFORM).transformBy(ROBOT_TRANSFORM), true).get();
         }
 
         public Pose2d getPose() {
-            return Flippable.isRedAlliance() ? flipAboutBothAxis(facePose) : facePose;
+            return facePose;
         }
 
         public Pose2d getLeftBranch() {
-            return Flippable.isRedAlliance() ? flipAboutBothAxis(leftBranch) : leftBranch;
+            return leftBranch;
         }
 
         public Pose2d getRightBranch() {
-            return Flippable.isRedAlliance() ? flipAboutBothAxis(rightBranch) : rightBranch;
+            return rightBranch;
         }
     }
 }
