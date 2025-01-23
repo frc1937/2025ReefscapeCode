fb3ef10d3643216afa14d8e2fa5fa52af8e5b0e7
biqsweet <ranranlevyy@gmail.com>
2025-01-23 08:58:31 +0200

    Fix: added an import I forgot.

--- src/main/java/frc/robot/commands/PathfindingCommands.java
+++ src/main/java/frc/robot/commands/PathfindingCommands.java
@@ -5,7 +5,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.DeferredCommand;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.lib.util.flippable.Flippable;
 import frc.lib.util.flippable.FlippablePose2d;
