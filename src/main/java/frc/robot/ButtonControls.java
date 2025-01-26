c06061df61df2e4d5526aeedecb9d52235a71e49
itsWihy <devwihy@gmail.com>
2025-01-26 23:42:32 +0200

    Refactor: Cleaned up button control, added elevator controls

---
+++ src/main/java/frc/robot/ButtonControls.java
@@ -0,0 +1,106 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import frc.lib.generic.GenericSubsystem;
+import frc.lib.generic.characterization.WheelRadiusCharacterization;
+import frc.lib.generic.hardware.KeyboardController;
+import frc.lib.util.Controller;
+import frc.lib.util.flippable.Flippable;
+import frc.robot.commands.PathfindingCommands;
+import frc.robot.subsystems.elevator.ElevatorConstants;
+import frc.robot.subsystems.swerve.SwerveCommands;
+
+import java.util.function.DoubleSupplier;
+
+import static frc.lib.util.Controller.Axis.LEFT_X;
+import static frc.lib.util.Controller.Axis.LEFT_Y;
+import static frc.robot.RobotContainer.ELEVATOR;
+import static frc.robot.RobotContainer.SWERVE;
+import static frc.robot.commands.PathfindingCommands.pathfindToFeeder;
+import static frc.robot.utilities.PathPlannerConstants.ROBOT_CONFIG;
+
+public class ButtonControls {
+    public enum ButtonLayout {
+        TELEOP,
+        CHARACTERIZE_ELEVATOR,
+        CHARACTERIZE_SWERVE,
+        CHARACTERIZE_WHEEL_RADIUS,
+        CHARACTERIZE_ARM
+    }
+
+    private static final Controller DRIVER_CONTROLLER = new Controller(0);
+    private static final KeyboardController OPERATOR_CONTROLLER = new KeyboardController();
+
+    private static final Trigger USER_BUTTON = new Trigger(RobotController::getUserButton);
+
+    public static void initializeButtons(ButtonLayout layout) {
+        switch (layout) {
+            case TELEOP -> configureButtonsTeleop();
+            case CHARACTERIZE_WHEEL_RADIUS -> configureButtonsCharacterizeWheelRadius();
+        }
+    }
+
+    private static void configureButtonsTeleop() {
+        DoubleSupplier driveSign = () -> Flippable.isRedAlliance() ? 1 : -1;
+
+        DoubleSupplier translationSupplier = () -> driveSign.getAsDouble() * DRIVER_CONTROLLER.getRawAxis(LEFT_Y);
+        DoubleSupplier strafeSupplier = () -> driveSign.getAsDouble() * DRIVER_CONTROLLER.getRawAxis(LEFT_X);
+        DoubleSupplier rotationSupplier = () -> driveSign.getAsDouble() * DRIVER_CONTROLLER.getRawAxis(Controller.Axis.RIGHT_X);
+
+        SWERVE.setDefaultCommand(
+                SwerveCommands.driveOpenLoop(
+                        translationSupplier,
+                        strafeSupplier,
+                        rotationSupplier,
+
+                        () -> DRIVER_CONTROLLER.getStick(Controller.Stick.RIGHT_STICK).getAsBoolean()
+                ));
+
+        DRIVER_CONTROLLER.getButton(Controller.Inputs.START).whileTrue(SwerveCommands.resetGyro());
+        DRIVER_CONTROLLER.getButton(Controller.Inputs.BACK).whileTrue(SwerveCommands.lockSwerve());
+
+        DRIVER_CONTROLLER.getButton(Controller.Inputs.LEFT_BUMPER).whileTrue(
+                PathfindingCommands.pathfindToLeftBranch().alongWith(ELEVATOR.setTargetHeight(ElevatorConstants.ElevatorHeight.L2))
+        );
+
+        DRIVER_CONTROLLER.getButton(Controller.Inputs.RIGHT_BUMPER).whileTrue(
+                PathfindingCommands.pathfindToRightBranch().alongWith(ELEVATOR.setTargetHeight(ElevatorConstants.ElevatorHeight.L2))
+        );
+
+        //TODO: Figure out how to access buttons on back of Kingston and use it here INSTEAD!
+        DRIVER_CONTROLLER.getButton(Controller.Inputs.X).whileTrue(
+                pathfindToFeeder().alongWith(ELEVATOR.setTargetHeight(ElevatorConstants.ElevatorHeight.FEEDER))
+        );
+
+        DRIVER_CONTROLLER.getStick(Controller.Stick.LEFT_STICK).whileTrue(
+                ELEVATOR.setTargetHeight(ElevatorConstants.ElevatorHeight.L1)
+        );
+
+        DRIVER_CONTROLLER.getStick(Controller.Stick.RIGHT_STICK).whileTrue(
+                ELEVATOR.setTargetHeight(ElevatorConstants.ElevatorHeight.L3)
+        );
+    }
+
+
+    private static void configureButtonsCharacterizeWheelRadius() {
+        final Command wheelRadiusCharacterization = new WheelRadiusCharacterization(
+                SWERVE,
+                ROBOT_CONFIG.moduleLocations,
+                SWERVE::getDriveWheelPositionsRadians,
+                () -> SWERVE.getGyroHeading() * 2 * Math.PI,
+                SWERVE::runDriveMotorWheelCharacterization
+        );
+
+        DRIVER_CONTROLLER.getButton(Controller.Inputs.A).whileTrue(wheelRadiusCharacterization);
+    }
+
+    private void setupSysIdCharacterization(GenericSubsystem subsystem) {
+        DRIVER_CONTROLLER.getButton(Controller.Inputs.A).whileTrue(subsystem.getSysIdQuastatic(SysIdRoutine.Direction.kForward));
+        DRIVER_CONTROLLER.getButton(Controller.Inputs.B).whileTrue(subsystem.getSysIdQuastatic(SysIdRoutine.Direction.kReverse));
+        DRIVER_CONTROLLER.getButton(Controller.Inputs.Y).whileTrue(subsystem.getSysIdDynamic(SysIdRoutine.Direction.kForward));
+        DRIVER_CONTROLLER.getButton(Controller.Inputs.X).whileTrue(subsystem.getSysIdDynamic(SysIdRoutine.Direction.kReverse));
+    }
+}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,36 +1,18 @@
 package frc.robot;
 
 import com.pathplanner.lib.auto.AutoBuilder;
-import com.pathplanner.lib.commands.FollowPathCommand;
-import com.pathplanner.lib.commands.PathfindingCommand;
-import com.pathplanner.lib.path.PathPlannerPath;
-import edu.wpi.first.math.geometry.Rotation3d;
-import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
-import frc.lib.generic.GenericSubsystem;
-import frc.lib.util.Controller;
 import frc.lib.util.flippable.Flippable;
 import frc.robot.poseestimation.poseestimator.PoseEstimator;
 import frc.robot.subsystems.elevator.Elevator;
-import frc.robot.subsystems.elevator.ElevatorConstants.*;
 import frc.robot.subsystems.leds.Leds;
 import frc.robot.subsystems.swerve.Swerve;
-import frc.robot.subsystems.swerve.SwerveCommands;
-import org.json.simple.parser.ParseException;
 import frc.robot.utilities.PathPlannerConstants;
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
-import java.io.IOException;
-import java.util.function.DoubleSupplier;
-
-import static frc.lib.util.Controller.Axis.LEFT_X;
-import static frc.lib.util.Controller.Axis.LEFT_Y;
-import static frc.robot.commands.PathfindingCommands.setupFeederPathfinding;
 import static frc.robot.poseestimation.poseestimator.PoseEstimatorConstants.*;
 
 public class RobotContainer {
@@ -42,21 +24,21 @@ public class RobotContainer {
     );
 
     public static final Swerve SWERVE = new Swerve();
-    public static final Leds LEDS = new Leds();
     public static final Elevator ELEVATOR = new Elevator();
-
-    private final Trigger userButton = new Trigger(RobotController::getUserButton);
-
-    private final Controller driveController = new Controller(0);
+    public static final Leds LEDS = new Leds();
 
     private LoggedDashboardChooser<Command> autoChooser;
 
     public RobotContainer() {
+        DriverStation.silenceJoystickConnectionWarning(true);
+
         Flippable.init();
         PathPlannerConstants.initializePathPlanner();
 
         setupAutonomous();
-        configureBindings();
+        setupLEDs();
+
+        ButtonControls.initializeButtons(ButtonControls.ButtonLayout.TELEOP);
     }
 
     public Command getAutonomousCommand() {
@@ -67,93 +49,23 @@ public class RobotContainer {
         return autoChooser.getSendableChooser().getSelected();
     }
 
-    private void configureBindings() {
-        DriverStation.silenceJoystickConnectionWarning(true);
-
-        setupLEDs();
-
-        setupFeederPathfinding(driveController.getButton(Controller.Inputs.A));
-
-        driveController.getButton(Controller.Inputs.B).whileTrue(ELEVATOR.setTargetPosition(ElevatorHeight.CLIMB));
-        driveController.getButton(Controller.Inputs.A).whileTrue(ELEVATOR.setTargetPosition(ElevatorHeight.L1));
-        driveController.getButton(Controller.Inputs.Y).whileTrue(ELEVATOR.setTargetPosition(ElevatorHeight.L3));
-        driveController.getButton(Controller.Inputs.X).whileTrue(ELEVATOR.setTargetPosition(ElevatorHeight.FEEDER));
-
-        ELEVATOR.setDefaultCommand(
-                ELEVATOR.setTargetPosition(ElevatorHeight.L2)
-        );
-
-        configureButtons(ButtonLayout.TELEOP);
-    }
-
-    private void configureButtons(ButtonLayout layout) {
-        switch (layout) {
-            case TELEOP -> configureButtonsTeleop();
-        }
-    }
-
-    private void setupCharacterization(GenericSubsystem subsystem) {
-        driveController.getButton(Controller.Inputs.A).whileTrue(subsystem.getSysIdQuastatic(SysIdRoutine.Direction.kForward));
-        driveController.getButton(Controller.Inputs.B).whileTrue(subsystem.getSysIdQuastatic(SysIdRoutine.Direction.kReverse));
-        driveController.getButton(Controller.Inputs.Y).whileTrue(subsystem.getSysIdDynamic(SysIdRoutine.Direction.kForward));
-        driveController.getButton(Controller.Inputs.X).whileTrue(subsystem.getSysIdDynamic(SysIdRoutine.Direction.kReverse));
-    }
-
     private void setupLEDs() {
         LEDS.setDefaultCommand(LEDS.setLEDStatus(Leds.LEDMode.DEFAULT, 0));
 
         final int LOW_BATTERY_THRESHOLD = 150;
         final int[] lowBatteryCounter = {0};
 
-        new Trigger(() -> {
-            if (RobotController.getBatteryVoltage() < 11.7) lowBatteryCounter[0]++;
+        final Trigger batteryLowTrigger = new Trigger(() -> {
+            if (RobotController.getBatteryVoltage() < 11.7)
+                lowBatteryCounter[0]++;
+
             return LOW_BATTERY_THRESHOLD < lowBatteryCounter[0];
-        }).onTrue(LEDS.setLEDStatus(Leds.LEDMode.BATTERY_LOW, 5));
-    }
+        });
 
-    private void setupDriving(DoubleSupplier translationSupplier, DoubleSupplier strafeSupplier, DoubleSupplier rotationSupplier) {
-        SWERVE.setDefaultCommand(
-                SwerveCommands.driveOpenLoop(
-                        translationSupplier,
-                        strafeSupplier,
-                        rotationSupplier,
-
-                        () -> driveController.getStick(Controller.Stick.RIGHT_STICK).getAsBoolean()
-                ));
-
-        driveController.getButton(Controller.Inputs.START).whileTrue(SwerveCommands.resetGyro());
-        driveController.getButton(Controller.Inputs.BACK).whileTrue(SwerveCommands.lockSwerve());
-    }
-
-    private void configureButtonsTeleop() {
-        DoubleSupplier driveSign = () -> Flippable.isRedAlliance() ? 1 : -1;
-
-        DoubleSupplier translationSupplier = () -> driveSign.getAsDouble() * driveController.getRawAxis(LEFT_Y);
-        DoubleSupplier strafeSupplier = () -> driveSign.getAsDouble() * driveController.getRawAxis(LEFT_X);
-        DoubleSupplier rotationSupplier =  () -> driveSign.getAsDouble() * driveController.getRawAxis(Controller.Axis.RIGHT_X);
-
-        setupDriving(translationSupplier, strafeSupplier, rotationSupplier);
-
-//        driveController.getButton(Controller.Inputs.A)
-//            .whileTrue(SwerveCommands.driveAndRotateToClosestNote(translationSupplier, strafeSupplier));
-
-
-//        driveController.getButton(Controller.Inputs.B).whileTrue(
-//                new WheelRadiusCharacterization(
-//                        SWERVE,
-//                        MODULE_LOCATIONS,
-//                        SWERVE::getDriveWheelPositionsRadians,
-//                        () -> SWERVE.getGyroHeading().getRadians(),
-//                        SWERVE::runWheelCharacterization
-//                ));
-//        );
+        batteryLowTrigger.onTrue(LEDS.setLEDStatus(Leds.LEDMode.BATTERY_LOW, 5));
     }
 
     private void setupAutonomous() {
         autoChooser = new LoggedDashboardChooser<>("AutoChooser", AutoBuilder.buildAutoChooser(""));
-    }
-
-    private enum ButtonLayout {
-        TELEOP
     }
 }
