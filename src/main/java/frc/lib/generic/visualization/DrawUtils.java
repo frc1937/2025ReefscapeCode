321612c2180c46f7886bf5e8c53ebc11a767d15d
biqsweet <ranranlevyy@gmail.com>
2025-02-02 02:43:05 +0200

    Fix: moved to lib and added drawLine

--- src/main/java/frc/lib/generic/visualization/DrawUtils.java
+++ src/main/java/frc/lib/generic/visualization/DrawUtils.java
@@ -1,4 +1,4 @@
-package frc.robot.utilities;
+package frc.lib.generic.visualization;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -12,8 +12,9 @@ public class DrawUtils {
      * @param radius The radius of the circle.
      * @param centerPose The center pose of the circle.
      * @param numPoints The number of points to approximate the circle.
+     * @param name The name as it's seen in AdvantageScope
      */
-    public static void drawCircle(double radius, Pose2d centerPose, int numPoints) {
+    public static void drawCircle(double radius, Pose2d centerPose, int numPoints, String name) {
         final Pose2d[] circlePoses = new Pose2d[numPoints];
         final double twoPi = 2 * Math.PI;
 
@@ -25,14 +26,29 @@ public class DrawUtils {
                     radius * Math.sin(theta)
             ).rotateBy(centerPose.getRotation());
 
-
             circlePoses[i] = new Pose2d(
                     centerPose.getTranslation().plus(radial),
                     new Rotation2d(radial.getX(), radial.getY()).plus(Rotation2d.fromDegrees(90))
             );
         }
 
-        Logger.recordOutput("DrawingUtils/", circlePoses);
+        Logger.recordOutput("DrawingUtils/" + name, circlePoses);
+    }
+
+    /**
+     * Draws a line between two points.
+     *
+     * @param startPoint The starting point of the line.
+     * @param endPoint The ending point of the line.
+     * @param name The name as it's seen in AdvantageScope
+     */
+    public static void drawLine(Translation2d startPoint, Translation2d endPoint, String name) {
+        Pose2d[] linePoses = new Pose2d[2];
+
+        linePoses[0] = new Pose2d(startPoint, Rotation2d.kZero);
+        linePoses[1] = new Pose2d(endPoint,Rotation2d.kZero);
+
+        Logger.recordOutput("DrawingUtils/" + name, linePoses);
     }
 
 }
