9c49505c2b1c253a2db34e0e9c6b1e2594d0e1a1
itsWihy <devwihy@gmail.com>
2025-02-25 19:39:57 +0200

    Feat: added untested cage alignment

--- src/main/java/frc/robot/commands/ClimbingCommands.java
+++ src/main/java/frc/robot/commands/ClimbingCommands.java
@@ -1,10 +1,15 @@
 package frc.robot.commands;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.DeferredCommand;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.lib.util.flippable.FlippableRotation2d;
 import frc.robot.commands.pathfinding.PathfindingCommands;
 import frc.robot.subsystems.elevator.ElevatorConstants;
+import frc.robot.subsystems.swerve.Swerve;
+import frc.robot.subsystems.swerve.SwerveCommands;
+import frc.robot.subsystems.swerve.SwerveConstants;
 
 import static frc.robot.RobotContainer.ELEVATOR;
 
@@ -15,12 +20,28 @@ public class ClimbingCommands {
         return pathfindingCommand
                 .alongWith(ELEVATOR.setTargetHeight(ElevatorConstants.ElevatorHeight.L2))
                 .until(pathfindingCommand::isFinished)
-                .andThen(ELEVATOR.setTargetHeight(ElevatorConstants.ElevatorHeight.CLIMB));
+                .andThen(ELEVATOR.setTargetHeight(ElevatorConstants.ElevatorHeight.CLIMB_LOWERING));
     }
 
     public static Command climbCageNoAlignment() {
         return (ELEVATOR.setTargetHeight(ElevatorConstants.ElevatorHeight.L2))
+                .raceWith(new WaitCommand(1.8))
+                .andThen(ELEVATOR.setTargetHeight(ElevatorConstants.ElevatorHeight.CLIMB_LOWERING));
+    }
+
+    //UNTESTED
+    public static Command rotateToCage() {
+        //PUT elevator at L2
+        //Rotate so elevator is facing the cage (FROM BEHIND)
+        //Drive forward a bit.
+        //drive backwards a bit
+        //drive elevator downwards.
+        return SwerveCommands.rotateToTarget(new FlippableRotation2d(Rotation2d.kPi, true))
+                .alongWith(ELEVATOR.setTargetHeight(ElevatorConstants.ElevatorHeight.L2))
+                .andThen(SwerveCommands.driveOpenLoop(() -> 0.5, () -> 0, () -> 0, () -> true))
                 .raceWith(new WaitCommand(0.8))
-                .andThen(ELEVATOR.setTargetHeight(ElevatorConstants.ElevatorHeight.CLIMB));
+                .andThen(SwerveCommands.driveOpenLoop(() -> -0.5, () -> 0, () -> 0, () -> true))
+                .raceWith(new WaitCommand(0.5))
+                .andThen(ELEVATOR.setTargetHeight(ElevatorConstants.ElevatorHeight.CLIMB_LOWERING));
     }
 }
